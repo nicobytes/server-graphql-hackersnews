ede5e3182416cbcd73c5f36bc9efdfa23a03212d
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-11-29 10:33:00 -0400

    implement resolvers

--- schema/Link.js
+++ schema/Link.js
@@ -1,5 +1,7 @@
 const LinkDef = `
+# Representa un link
 type Link {
+  # Es el id unico
   id: ID!
   url: String!
   description: String!
--- schema/index.js
+++ schema/index.js
@@ -1,9 +1,16 @@
 const { makeExecutableSchema } = require('graphql-tools');
+const resolvers = require('./resolvers');
 
-const LinkDef = require('./Link')
+const LinkDef = require('./Link');
 
+const rootQuery = `
+type Query {
+  allLinks: [Link!]
+}
+`
 
 // Generate the schema object from your types definition.
 module.exports = makeExecutableSchema({
-  typeDefs: [LinkDef]
+  typeDefs: [rootQuery, LinkDef],
+  resolvers
 });
